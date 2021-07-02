1d73cef52459daf35c129692ff195226f0eec5cf
nkadebug <nka.debug@gmail.com>
2021-07-02 17:03:13 +0000

    update

--- public/main.js
+++ public/main.js
@@ -1,19 +1,34 @@
 let auth = firebase.auth();
 let db = firebase.database();
 
+const usersRef = db.ref('users');
+const chatsRef = db.ref('chats');
+
+let uid = null;
+let to = 'nka';
+
 auth.signInAnonymously();
 
 auth.onAuthStateChanged((user) => {
-    if (user) {
-        console.log(user.uid);
-    }
+  if (user) {
+    uid = user.uid;
+    usersRef.child(uid).child('status').set('Online');
+    usersRef
+      .child(uid)
+      .child('status')
+      .onDisconnect()
+      .set('Offline at ' + new Date().getTime());
+  } else {
+    uid = null;
+  }
 });
 
 $('#chatbox').on('submit', function (e) {
-    e.preventDefault();
-    let msg = $('#msg').val();
-    if (msg) {
-        $('#msg').val('');
-        $('#chats').append(`<p>${msg}</p>`);
-    }
+  e.preventDefault();
+  let msg = $('#msg').val();
+  if (msg) {
+    $('#msg').val('');
+    $('#chats').append(`<p class='right'>${msg}</p>`);
+    chatsRef.push({ msg, from: uid, to, ts: new Date().getTime() });
+  }
 });
